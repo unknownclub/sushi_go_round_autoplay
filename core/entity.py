8aa34bc5e1d048dda98125d638c80c2106b211c6
un4ckn0wl3z <slickerth@gmail.com>
2020-08-16 16:57:24 +0700

    add entity pixel

---
+++ core/entity.py
@@ -0,0 +1,24 @@
+class Entity:
+    shrimp = (304, 695)
+    rice = (362, 696)
+    nori = (308, 743)
+    roe = (359, 750)
+    salmon = (310, 800)
+    eel = (357, 800)
+
+    phone = (837, 710)
+
+    phone_topping = (794, 635)
+    phone_rice = (785, 660)
+    phone_sake = (789, 684)
+
+    topping_shrimp = (770, 587)
+    topping_unagi = (854, 587)
+    topping_nori = (770, 648)
+    topping_fish_egg = (845, 642)
+    topping_salmon = (760, 694)
+    topping_back = (831, 692)
+
+    rice_rice = (814, 648)
+
+    sake_sake = (810, 637)
